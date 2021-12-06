24823a65bd4f7ad6bc7dbbe45fa454e4e47f22a5
OndrejValla <ondrej.valla@gmail.com>
2021-12-06 21:07:43 +0000

    Created home app and templates

--- z_v_gallery/settings.py
+++ z_v_gallery/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 
 ]
 
@@ -62,7 +63,10 @@ ROOT_URLCONF = 'z_v_gallery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

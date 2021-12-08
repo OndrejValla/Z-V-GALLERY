ca85c772446b4514754872c06699c705c7e28904
OndrejValla <ondrej.valla@gmail.com>
2021-12-08 14:37:29 +0000

    Added products app

---
+++ products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image1 = models.ImageField(null=True, blank=True)
+    image2 = models.ImageField(null=True, blank=True)
+    image3 = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name

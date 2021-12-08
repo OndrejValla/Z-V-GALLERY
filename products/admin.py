28c1baeca52fcc8040939f1f87b3a84010f3ae08
OndrejValla <ondrej.valla@gmail.com>
2021-12-08 21:35:20 +0000

    Customized Admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Project
 
 # Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'project',
+        'price',
+        'image1',
+        'image2',
+        'image3',
+    )
+
+    ordering = ('project',)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Project, ProjectAdmin)
--- products/models.py
+++ products/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
+
+class Project(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Product(models.Model):
+    project = models.ForeignKey(
+        'Project', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)

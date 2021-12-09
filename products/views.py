e8dc8b9c04e92e05aa7385940ec41e38f21f3652
OndrejValla <ondrej.valla@gmail.com>
2021-12-09 20:28:55 +0000

    Created product details function

--- products/views.py
+++ products/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def all_products(request):
-    """ A view to show all products """
+    """ A view to show each product details """
 
     products = Product.objects.all()
 
@@ -11,3 +11,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show all products """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)

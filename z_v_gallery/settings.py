a9ca64c22bd320b254d88299bc7c9fdae881a376
OndrejValla <ondrej.valla@gmail.com>
2021-12-14 17:31:40 +0000

    Added bag context

--- z_v_gallery/settings.py
+++ z_v_gallery/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'bag.contexts.bag_content',
             ],
         },
     },
@@ -157,6 +158,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STANDARD_DELIVERY_PERCENTAGE = 5
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

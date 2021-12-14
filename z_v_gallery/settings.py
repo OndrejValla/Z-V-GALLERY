67598dc419bab61cea21c47cf0e3b413cb63bc4f
OndrejValla <ondrej.valla@gmail.com>
2021-12-14 19:48:19 +0000

    Created Add to Bag functionality

--- z_v_gallery/settings.py
+++ z_v_gallery/settings.py
@@ -77,7 +77,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'bag.contexts.bag_content',
+                'bag.contexts.bag_contents',
             ],
         },
     },

2a7cad257c2db7b18209c8e5234978dd2473f13b
OndrejValla <ondrej.valla@gmail.com>
2021-12-15 21:43:36 +0000

    Added frame option to models and frame selector to product_detail.html

--- products/models.py
+++ products/models.py
@@ -19,6 +19,7 @@ class Product(models.Model):
         'Project', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    framed = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image1 = models.ImageField(null=True, blank=True)
     image2 = models.ImageField(null=True, blank=True)

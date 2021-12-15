770dd42a192cb36459896eaa067f5f0bf2b1595d
OndrejValla <ondrej.valla@gmail.com>
2021-12-15 23:02:24 +0000

    Added Product Frame selection

--- bag/views.py
+++ bag/views.py
@@ -12,12 +12,24 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    frame = None
+    if 'product_frame' in request.POST:
+        frame = request.POST['product_frame']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if frame:
+        if item_id in list(bag.keys()):
+            if frame in bag[item_id]['items_by_frame'].keys():
+                bag[item_id]['items_by_frame'][frame] += quantity
+            else:
+                bag[item_id]['items_by_frame'][frame] = quantity
+        else:
+            bag[item_id] = {'items_by_frame': {frame: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)

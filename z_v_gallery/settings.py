650f19ad63453146d6e455f82a693b01dcf87fa1
OndrejValla <ondrej.valla@gmail.com>
2021-12-22 00:46:06 +0000

    Added checkout views and templates

--- z_v_gallery/settings.py
+++ z_v_gallery/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
 
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'z_v_gallery.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -80,6 +84,10 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]

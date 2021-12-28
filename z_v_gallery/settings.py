195e352a47b908e1dcb8f553a59ec8b6521656a4
OndrejValla <ondrej.valla@gmail.com>
2021-12-28 20:33:14 +0000

    Added confirmation emails

--- z_v_gallery/settings.py
+++ z_v_gallery/settings.py
@@ -178,3 +178,4 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'zuzuvalla@gmail.com'

7603e45bad623853c68154de2f73edd01e801664
OndrejValla <ondrej.valla@gmail.com>
2021-12-29 23:06:45 +0000

    Product Management Add, Edit, Delete completed

--- products/forms.py
+++ products/forms.py
@@ -9,8 +9,6 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         projects = Project.objects.all()

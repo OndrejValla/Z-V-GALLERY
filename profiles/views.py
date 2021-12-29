acf6b9a0d57d4654a9582556f14a93f50b590d97
OndrejValla <ondrej.valla@gmail.com>
2021-12-29 18:38:43 +0000

    Added Add product functionality ...

--- profiles/views.py
+++ profiles/views.py
@@ -14,9 +14,11 @@ def profile(request):
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile successfully updated!')
-
-    form = UserProfileForm(instance=profile)
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'

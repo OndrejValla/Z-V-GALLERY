4a07256cf9b36044bb2705e7d68c4d3f7f689715
OndrejValla <ondrej.valla@gmail.com>
2021-12-30 22:15:55 +0000

    Added cache control

--- z_v_gallery/settings.py
+++ z_v_gallery/settings.py
@@ -181,6 +181,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'z-v-gallery'
     AWS_S3_REGION_NAME = 'eu-west-2'
@@ -197,6 +203,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 
 # Stripe
 STANDARD_DELIVERY_PERCENTAGE = 5

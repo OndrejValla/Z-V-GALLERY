b3cf5a8e1c757553073962869e431324c3b2c31f
OndrejValla <ondrej.valla@gmail.com>
2021-12-30 00:35:38 +0000

    SECRET KEY updated

--- z_v_gallery/settings.py
+++ z_v_gallery/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY_ZV', '')
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['z-v-gallery.herokuapp.com', 'localhost']
 

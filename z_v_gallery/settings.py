8acf851960a1b33f9c06d5287a989bbf55344742
OndrejValla <ondrej.valla@gmail.com>
2022-01-02 21:45:02 +0000

    Created Gallery section on the home page

--- z_v_gallery/settings.py
+++ z_v_gallery/settings.py
@@ -222,3 +222,8 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
